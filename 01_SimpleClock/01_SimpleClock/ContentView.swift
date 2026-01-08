0c8deb596ea37cf43df211e1291aab793789c387
LuoQiuSheng <bulustrive@sina.com>
2026-01-08 14:15:04 +0800

    【激活输入框后，再次点击输入框，输入框会收起】 https://www.tapd.cn/tapd_fe/55927117/bug/detail/1155927117001004353

---
+++ 01_SimpleClock/01_SimpleClock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01_SimpleClock
+//
+//  Created by Metalien on 2026/1/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
